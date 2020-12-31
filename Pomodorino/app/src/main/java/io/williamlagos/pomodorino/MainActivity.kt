90d50d887c826727f6c7c54c6ec3f56d1630825e
William Oliveira de Lagos <william.lagos@icloud.com>
2020-12-31 18:50:42 +0000

    Updated with internal timer and added binding activity option

--- Pomodorino/app/src/main/java/io/williamlagos/pomodorino/MainActivity.kt
+++ Pomodorino/app/src/main/java/io/williamlagos/pomodorino/MainActivity.kt
@@ -1,13 +1,33 @@
 package io.williamlagos.pomodorino
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import io.williamlagos.pomodorino.databinding.ActivityMainBinding
+import java.util.*
+
+internal class Helper : TimerTask() {
+
+    override fun run() {
+        Log.i("Timer", "Timer ran " + ++i)
+    }
+
+    companion object {
+        var i = 0
+    }
+}
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.timerLabel.setText("Timer started")
         var t = Timer(true)
-        timerLabel.setText(t.toString())
+        t.schedule(Helper(), 1000, 5000)
     }
 }
