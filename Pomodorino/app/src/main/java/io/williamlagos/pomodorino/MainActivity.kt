9f08ca55c0f7a1c194aabcdd531de0d5e84deba2
William Oliveira de Lagos <william.lagos@icloud.com>
2020-12-31 17:58:51 +0000

    Added countdownTimer class for doing the base for the android app

--- Pomodorino/app/src/main/java/io/williamlagos/pomodorino/MainActivity.kt
+++ Pomodorino/app/src/main/java/io/williamlagos/pomodorino/MainActivity.kt
@@ -1,33 +1,36 @@
 package io.williamlagos.pomodorino
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
-import android.widget.Toast
+//import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.williamlagos.pomodorino.databinding.ActivityMainBinding
-import java.util.*
-
-internal class Helper : TimerTask() {
-
-    override fun run() {
-        Log.i("Timer", "Timer ran " + ++i)
-    }
-
-    companion object {
-        var i = 0
-    }
-}
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
+    private var counter = 0
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-        binding.timerLabel.setText("Timer started")
-        var t = Timer(true)
-        t.schedule(Helper(), 1000, 5000)
+        setContentView(binding.root)
+        val pomodoroMillis = 1_500_000L
+        val timer = object: CountDownTimer(pomodoroMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                var secondsUntilFinished = millisUntilFinished / 1000
+                binding.timerLabel.text = "Timer: $secondsUntilFinished."
+            }
+
+            override fun onFinish() {
+                binding.timerLabel.text = "Timer ran out."
+//                Log.i("Timer", "Timer ran out.")
+            }
+        }
+        timer.start()
     }
 }
